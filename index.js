e361f970974f5cca37f2e9c7bc39d12a9646d718
sahinealam <msahine961@gmail.com>
2026-04-24 09:06:33 +0600

    full error slove

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe =require("stripe")(process.env.STRIPE_SECATE);
+const crypto=require("crypto")
 // const port =3000;
 app.use(cors());
 app.use(express.json());
@@ -115,6 +117,11 @@ async function run() {
       const totalRequest = await requestCollection.countDocuments();
       res.send({ requests: result, total: totalRequest });
     });
+
+    // payments
+    app.post('/create-payment-intent',async(req,res)=>{
+      
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
